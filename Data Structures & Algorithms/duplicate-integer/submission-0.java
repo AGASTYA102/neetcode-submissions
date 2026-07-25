df751d1beeb685e38b6f70092ece34738d51889a
Agastya Bhatia <agastya.mitmpl2024@learner.manipal.edu>
2026-07-25 14:49:26 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+    	HashSet<Integer> set = new HashSet<>();
+    	for(int x : nums) {
+    		if(!set.add(x)) {
+    			return true;
+    		}
+    		set.add(x);
+    	}
+        return false;
+    }
+}
